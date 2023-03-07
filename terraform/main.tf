ef458b1d7e86b15201c3f9b88452313856afde32
Thomas Pisula <tpisula@labrats.work>
2023-03-07 17:54:28 +0100

    testing

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,7 +18,7 @@ resource "azurerm_ssh_public_key" "sysadmin" {
   resource_group_name = azurerm_resource_group.main.name
 }
 
-resource "azurerm_subnet" "internal" {
+  resource "azurerm_subnet" "internal" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
