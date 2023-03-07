da7d028cee35f31737274c330535f531dcf44bce
Thomas Pisula <tpisula@labrats.work>
2023-03-07 16:51:42 +0100

    wip

---
+++ terraform/main.tf
@@ -0,0 +1,89 @@
+resource "azurerm_resource_group" "main" {
+  name     = "${var.prefix}-resources"
+  location = var.location
+}
+
+resource "azurerm_ssh_public_key" "sysadmin" {
+  name                = "${var.prefix}-key"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  public_key          = file("~/.ssh/id_rsa.pub")
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_public_ip" "main" {
+  name                = "${var.prefix}-pip"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.main.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "main" {
+  name                = "${var.prefix}-vm"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  size                = "Standard_B1s"
+
+  admin_username = "sysadmin"
+  admin_ssh_key {
+    username   = "sysadmin"
+    public_key = azurerm_ssh_public_key.sysadmin.public_key
+  }
+
+  disable_password_authentication = true
+  network_interface_ids = [
+    azurerm_network_interface.main.id,
+  ]
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "ls -la /tmp",
+    ]
+
+    connection {
+      type  = "ssh"
+      agent = "true"
+      user  = self.admin_username
+      host  = self.public_ip_address
+      port  = "22"
+    }
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,25 @@
+variable "prefix" {
+  description = "The prefix which should be used for all resources in this example"
+  default     = "weather"
+}
+
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be created."
+  default     = "West Europe"
+}
+
+variable "tenant_id" {
+  description = "azure rm tenant id."
+}
+
+variable "subscription_id" {
+  description = "azure rm subscription id."
+}
+
+variable "client_id" {
+  description = "azure rm client id."
+}
+
+variable "client_secret" {
+  description = "azure rm client secret."
+}
