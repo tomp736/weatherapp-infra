2421f30ab8f77f6cdb6c3b0941640f0376d2d15c
Thomas Pisula <tpisula@labrats.work>
2023-03-09 12:58:44 +0100

    add domain_name_label

--- terraform/main.tf
+++ terraform/main.tf
@@ -31,6 +31,7 @@ resource "azurerm_public_ip" "main" {
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
   allocation_method   = "Static"
+  domain_name_label   = "${var.prefix}-vm"
 }
 
 resource "azurerm_network_interface" "main" {
